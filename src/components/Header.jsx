93c9e7a68860bf75bfd1a424febc1b6cdb5328f1
Yan Silitskiy <yanuck00723@gmail.com>
2021-04-05 10:44:39 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src='https://www.pinclipart.com/picdir/middle/379-3791327_pokemon-logos-png-vector-pokemon-logo-transparent-background.png'/>
+        </header>
+    );
+};
