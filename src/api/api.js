c19b13832b3f6b34902e75b1eb9e25017ca9b1e7
Yan Silitskiy <yanuck00723@gmail.com>
2021-05-31 19:21:56 +0300

    refactored UserContainer component, add DAL (lesson 63)

---
+++ src/api/api.js
@@ -0,0 +1,22 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+    baseURL: "https://social-network.samuraijs.com/api/1.0/",
+    withCredentials: true,
+    headers: {
+        "API-KEY": "bbaeb649-5dd8-4710-a4e8-3781d01da372"
+    }
+});
+
+export const usersAPI = {
+  getUsers(currentPage, pageSize) {
+      return instance
+          .get(`users?page=${currentPage}&count=${pageSize}`)
+          .then(response => response.data)
+  },
+  getUsersPagination(pageNumber, pageSize) {
+      return instance
+          .get(`users?page=${pageNumber}&count=${pageSize}`)
+          .then(response => response.data)
+  }
+};
